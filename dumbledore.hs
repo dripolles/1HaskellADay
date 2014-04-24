95f3e5f8dd2ef141f5b762db870de74f49f69b6e
Daniel Ripolles <dripolles@gmail.com>
2014-04-24 19:31:23 +0200

    Simplification

--- dumbledore.hs
+++ dumbledore.hs
@@ -1,4 +1,3 @@
-import Control.Applicative
 import Data.Tree
 
 dumbledore :: (a -> [b] -> b) -> Tree a -> b
@@ -8,7 +7,7 @@ height :: Tree a -> Int
 height = dumbledore $ \_ xs -> 1 + maximum (0:xs)
 
 height' :: Tree a -> Int
-height' = dumbledore $ const $ (1+) <$> maximum . (0:)
+height' = dumbledore $ const $ (1+) . maximum . (0:)
 
 main = do
     print $ height $ Node 4 [Node 5 [], Node 2 [Node 3 []]]
