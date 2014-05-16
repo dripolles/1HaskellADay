2e3b9f843e255e70f3e91eeb9dfe16d571e96359
Daniel Ripolles <dripolles@gmail.com>
2014-05-16 14:55:23 +0200

    Some prints for testing

--- shadok.hs
+++ shadok.hs
@@ -1,20 +1,23 @@
-data Gabuzomeu a b = Gabu a | Zomeu b
+data Gabuzomeu a b = Gabu a | Zomeu b deriving Show
 
 shadok :: (a -> c) -> (b -> d) -> Gabuzomeu a b -> Gabuzomeu c d
 shadok f _ (Gabu x) = Gabu $ f x
 shadok _ f (Zomeu x) = Zomeu $ f x
 
-newtype G b a = G {first :: Gabuzomeu a b}
+newtype G b a = G {first :: Gabuzomeu a b} deriving Show
 
 instance Functor (G b) where
   --fmap :: (a -> c) -> G b a -> G b c
   fmap f = G . (shadok f id ) . first
 
-newtype Z a b = Z {second :: Gabuzomeu a b}
+newtype Z a b = Z {second :: Gabuzomeu a b} deriving Show
 
 instance Functor (Z a) where
   -- fmap :: (b -> c) -> Z a b -> Z a c
   fmap f = Z . (shadok id f) . second
 
 main = do
-    print $ "OK"
+    print $ fmap (+2) (G (Gabu 2)::G Char Int)
+    print $ fmap (+2) (G (Zomeu 'a')::G Char Int)
+    print $ fmap succ (Z (Gabu 2)::Z Int Char)
+    print $ fmap succ (Z (Zomeu 'a')::Z Int Char)
